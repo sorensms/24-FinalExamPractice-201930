4f752b3cea65036359baafb197d95601ebfc1bf3
sorensms <sorensms@rose-hulman.edu>
2019-05-02 10:16:55 -0400

    Done

--- src/m1_sequence_mutation.py
+++ src/m1_sequence_mutation.py
@@ -6,8 +6,8 @@ This problem provides practice at:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Madie Sorensen.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -31,8 +31,26 @@ def run_test_zero_changer():
     print('  Expected:', expected1)
     print('  Actual:  ', test1)
 
+    # Test 2:
+    test2 = ([16, 0, 0, 4], [0, 0, 0, 0, 0], [12, 9,0, 4], [6,0,0, 0, 4])
+    expected2 = ([16, 1, 2, 4], [3, 4, 5, 6, 7], [12, 9, 8, 4], [6, 9, 10, 11, 4])
+    zero_changer(test2)
+    print()
+    print('Test 2:')
+    print('  Expected:', expected2)
+    print('  Actual:  ', test2)
+
+    # Test 3:
+    test3 = ([12, 5, 2, 1,6], [9,8,18, 0, 0, 1, 5, 0], [6, 9, 8], [6, 0,0,5, 4])
+    expected3 = ([12, 5, 2, 1, 6], [9, 8, 18, 1, 2, 1, 5, 3], [6, 9, 8], [6, 4, 5, 5, 4])
+    zero_changer(test3)
+    print()
+    print('Test 3:')
+    print('  Expected:', expected3)
+    print('  Actual:  ', test3)
+
     # -------------------------------------------------------------------------
-    # TODO: 2. Write at least 2 additional tests for the
+    # Done: 2. Write at least 2 additional tests for the
     #    zero_changer
     # function.  Try to choose some unexpected things like empty lists
     # or an empty tuple, or a list with no zeros, etc.
@@ -40,6 +58,14 @@ def run_test_zero_changer():
 
 
 def zero_changer(tuple_of_lists):
+    a=1
+    for k in range (len(tuple_of_lists)):
+        inside=tuple_of_lists[k]
+        for m in range(len(tuple_of_lists[k])):
+            if inside[m]==0:
+                inside[m]=a
+                a=a+1
+
     """
     What comes in:  A TUPLE of LISTs,
                     where the interior lists contain only integers.
@@ -64,7 +90,7 @@ def zero_changer(tuple_of_lists):
       :type tuple_of_lists: tuple of list[int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # Done: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # -------------------------------------------------------------------------
     # -------------------------------------------------------------------------
