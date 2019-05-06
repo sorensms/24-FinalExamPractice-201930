49de106c65a42b74604e164505733f1d22afa51a
sorensms <sorensms@rose-hulman.edu>
2019-05-06 11:28:30 -0400

    Done

--- src/m5_loops_within_loops_sequences.py
+++ src/m5_loops_within_loops_sequences.py
@@ -146,7 +146,7 @@ def integers(sequence_of_sequences):
 def run_test_big_letters():
     """ Tests the    big_letters    function. """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement this TEST function.
+    # Done: 4. Implement this TEST function.
     #   It TESTS the  big_letters  function defined below.
     #   Include at least ** 1 ** ADDITIONAL test beyond those we wrote.
     # -------------------------------------------------------------------------
@@ -181,9 +181,30 @@ def run_test_big_letters():
                           ])
     print('Expected is:', expected)
     print('Actual is:  ', answer)
+    # Test 2:
+    expected = 'HELLOBYEA'
+    answer = big_letters([(3, 1, 4),
+                          'HELLO there?',
+                          (10, 'DoES This WORK', 10),
+                          [],
+                          ['HI'],
+                          'hello',
+                          ['HoLA'],
+                          '1 BYE !',
+                          'A'])
+    print('Expected is:', expected)
+    print('Actual is:  ', answer)
 
 
 def big_letters(sequence_of_sequences):
+    new = ''
+    for k in range(len(sequence_of_sequences)):
+        short = sequence_of_sequences[k]
+        for m in range(len(short)):
+            if type(short)is  str:
+                if short[m].isupper() is True:
+                    new = new + short[m]
+    return new
     """
     Returns a new STRING that contains all the upper-case letters
     in the subsequences of the given sequence that are strings,
@@ -212,7 +233,7 @@ def big_letters(sequence_of_sequences):
     Precondition:  the given argument is a sequence of sequences.
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # Done: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # -------------------------------------------------------------------------
     ###########################################################################
@@ -234,7 +255,7 @@ def big_letters(sequence_of_sequences):
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:  12 minutes.
     # -------------------------------------------------------------------------
-
+"".isupper
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
